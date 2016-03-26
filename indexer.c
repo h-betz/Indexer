724a30f39e32545f9e8409ba8c96c5d443c8f046
h-betz <hhb311@gmail.com>
2016-03-25 20:50:44 -0400

    added a tokenize function to detect words

--- indexer.c
+++ indexer.c
@@ -6,6 +6,42 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+int ind = 0;
+
+/*char * tokenizer(char *string) {
+    
+    
+}*/
+
+void tokenize(char *string) {
+    
+    char c = string[ind];
+    char *token = malloc(strlen(string) + 1);
+    ind++;
+    int tokenIndex = 0;
+    
+    if (isalpha(c)) {
+        
+        token[tokenIndex] = c;
+        tokenIndex++;
+        c = string[ind];
+        ind++;
+        
+        while (c != '\0' && isalnum(c)) {
+            token[tokenIndex] = c;
+            tokenIndex++;
+            c = string[ind];
+            ind++;
+        }
+        
+        token[tokenIndex] = '\0';
+        printf("%s\n", token);
+    }
+    
+    free(token);
+    
+}
+
 void readDirect(char *path) {
     
     DIR *direct = opendir(path);
@@ -28,30 +64,22 @@ void readDirect(char *path) {
                 free(txt);
             }
             
-            
         }
         
     }
     
 }
 
-void printFiles(DIR *d, struct dirent *dent) {
-    
-    printf("\t[%s]\n", dent->d_name);
-    while ((dent = readdir(d)) != NULL) {
-        if (dent->d_type == DT_REG) {
-            printf("\t[%s]\n", dent->d_name); 
-        }
-    }
-    
-    
-}
 
 int main (int argc, char **argv) {
     
     char *txt = argv[1];
-        
-    readDirect(txt);
+    int length = strlen(txt);
+    printf("%d\n", length);
+    while (ind < length) {
+        tokenize(txt);            
+    }
+    //readDirect(txt);
     
     
     return 0;
