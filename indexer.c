d718ee04c5537bad7ff5bcd93d684b358b228622
h-betz <hhb311@gmail.com>
2016-03-29 20:38:49 -0400

    added a read file capability

--- indexer.c
+++ indexer.c
@@ -8,32 +8,28 @@
 
 int ind = 0;
 
-/*char * tokenizer(char *string) {
-    
-    
-}*/
-
 void tokenize(char *string) {
     
     char c = string[ind];
     char *token = malloc(strlen(string) + 1);
     ind++;
     int tokenIndex = 0;
+    int end = strlen(string);
     
     if (isalpha(c)) {
-        
+            
         token[tokenIndex] = c;
         tokenIndex++;
         c = string[ind];
         ind++;
-        
+                
         while (c != '\0' && isalnum(c)) {
             token[tokenIndex] = c;
             tokenIndex++;
             c = string[ind];
             ind++;
         }
-        
+            
         token[tokenIndex] = '\0';
         printf("%s\n", token);
     }
@@ -42,19 +38,50 @@ void tokenize(char *string) {
     
 }
 
+/*Reads the contents of the file and calls the tokenizer function to tokenize the contents*/
+void readFile(char *fileName) {
+    
+    FILE *f = fopen(fileName, "r");                         //open the file in read mode
+    char string[256];                                       //create a 256 byte char array to store the string in
+    if (f == NULL) {                                        //if failed to open the file, print error and return
+        printf("error\n");
+        return;
+    }
+    fgets(string, 255, (FILE*)f); 
+    int length = strlen(string);
+    while (ind < length) {   
+        tokenize(string);
+    }
+    fclose(f);
+    
+}
+
+
+/*Moves through the directory until it reaches the file or files
+  we are looking for
+ */
 void readDirect(char *path) {
     
-    DIR *direct = opendir(path);
-    struct dirent *dent;
+    DIR *direct = opendir(path);                                                    //open the directory
+    struct dirent *dent;                                                            //create a directory struct
         
-    if (direct != NULL) {
+    if (direct != NULL) {                                                           //if it's not a directory, our path name might be a file name
         
         while ((dent = readdir(direct)) != NULL) {
             
             if (!strcmp(dent->d_name, "..") || !strcmp(dent->d_name, ".")) {
-            
+                //do nothing if we see these symbols which mean previous directory and current directory
             } else if (dent->d_type == DT_REG) {
-                printf("\t[%s]\n", dent->d_name);              
+                
+                /*The following appends the file name to the end of the file/directory path*/
+                
+                char *string = malloc(strlen(dent->d_name) + strlen(path) + 2);
+                strcat(string, path);
+                strcat(string, "/");
+                strcat(string, dent->d_name);
+                readFile(string);                                                   //read the contents of the file
+                free(string);                                                       //free the memory allocated for the string
+                
             } else {
                 char *txt = malloc(strlen(path) + strlen(dent->d_name) + 2);
                 strcat(txt, path);
@@ -63,9 +90,15 @@ void readDirect(char *path) {
                 readDirect(txt);                  
                 free(txt);
             }
-            
         }
         
+    } else {
+        FILE *f = fopen(path, "r");                                                     //if we are given a file path, try to open the file
+        if (f == NULL) {
+            printf("Error! Failed to open file!\n");                                    //if failure, print an error and return
+            return;
+        }
+        readFile(path);                                                                 //call the readFile function
     }
     
 }
@@ -75,11 +108,7 @@ int main (int argc, char **argv) {
     
     char *txt = argv[1];
     int length = strlen(txt);
-    printf("%d\n", length);
-    while (ind < length) {
-        tokenize(txt);            
-    }
-    //readDirect(txt);
+    readDirect(txt);
     
     
     return 0;
